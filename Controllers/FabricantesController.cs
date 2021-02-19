5bc346fc354d6aa3b42592ffd3572229491168d8
Gilton Sampaio de Souza Júnior <gilton.j@escolar.ifrn.edu.br>
2021-02-19 00:41:03 -0300

    FIZ ESSE CACETE PORRA MERDA PALAVRÃO FALACIAS EU ESTOU MUITO FELIZ ESTOU A 9H NESTA DROGA MINHA MEDICAÇÃO ACABOU MAS DEU CERTO OBRIGADO JESUS CRISTO

--- Controllers/FabricantesController.cs
+++ Controllers/FabricantesController.cs
@@ -96,7 +96,9 @@ namespace PrimeiraAplicacao.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
+
         }
 
 
